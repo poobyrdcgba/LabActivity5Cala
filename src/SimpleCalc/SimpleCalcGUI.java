5f10e912a3cc9f34e6cb931907291b7b285e7646
CALA <robertdavid.cala@cit.edu>
2022-12-17 22:49:06 +0800

    Add SimpleCalcGUI main function

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -2,11 +2,23 @@ package SimpleCalc;
 
 import javax.swing.*;
 
-public class SimpleCalcGUI {
+public class SimpleCalcGUI extends JFrame {
     private JPanel panel1;
     private JTextField tfNumber1;
     private JComboBox cbOperations;
     private JButton btnCompute;
     private JTextField tfNumber2;
     private JTextField lblResult;
+
+    public static void main(String[] args) {
+
+        SimpleCalcGUI gwapo = new SimpleCalcGUI();
+        gwapo.setTitle("Simple Calculator");
+        gwapo.setContentPane(gwapo.panel1);
+        gwapo.setSize(1200, 700);
+        gwapo.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        gwapo.setVisible(true);
+        gwapo.lblResult.setEditable(false);
+
+    }
 }
