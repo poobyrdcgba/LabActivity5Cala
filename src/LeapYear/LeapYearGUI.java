d3e7983021b527dfe023a1470622ae899da97588
CALA <robertdavid.cala@cit.edu>
2022-12-17 21:50:43 +0800

    Complete LeapYearGUI

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -1,6 +1,8 @@
 package LeapYear;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class  LeapYearGUI extends JFrame{
     private JPanel panel1;
@@ -16,6 +18,21 @@ public class  LeapYearGUI extends JFrame{
         gwapo.setVisible(true);
 
     }
+    public LeapYearGUI() {
+            btnCheckYear.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    int input = Integer.parseInt(tfYear.getText());
+                    if(input % 4 == 0) {
+                    JOptionPane.showMessageDialog(panel1, "Leap year");
+                    }
+                    else {
+                        JOptionPane.showMessageDialog(panel1, "Not a leap year");
+                    }
+                }
+            });
+        }
 
     }
+
 }
