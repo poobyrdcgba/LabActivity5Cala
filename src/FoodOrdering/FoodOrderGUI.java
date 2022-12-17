20cb01b31e5b8cd4f05f314913e5fe5c37f1c050
CALA <robertdavid.cala@cit.edu>
2022-12-17 23:20:53 +0800

    Add FoodOrderGUI main function

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -2,7 +2,7 @@ package FoodOrdering;
 
 import javax.swing.*;
 
-public class FoodOrderGUI {
+public class FoodOrderGUI extends JFrame{
     private JPanel panel1;
     private JCheckBox cPizza;
     private JRadioButton rbNone;
@@ -15,4 +15,16 @@ public class FoodOrderGUI {
     private JRadioButton rb5;
     private JRadioButton rb10;
     private JRadioButton rb15;
+
+    public static void main(String[] args) {
+
+        SimpleCalcGUI gwapojapon = new SimpleCalcGUI();
+        gwapojapon.setTitle("Simple Calculator");
+        gwapojapon.setContentPane(gwapojapon.CalcuPanel);
+        gwapojapon.setSize(1280, 720);
+        gwapojapon.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        gwapojapon.setVisible(true);
+        gwapojapon.lblResult.setEditable(false);
+
+    }
 }
