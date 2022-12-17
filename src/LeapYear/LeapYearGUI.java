1ced6022079cec11cb3e560d028e0ef4943c3605
CALA <robertdavid.cala@cit.edu>
2022-12-17 21:40:26 +0800

    Add Leap Year main function

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -2,8 +2,20 @@ package LeapYear;
 
 import javax.swing.*;
 
-public class  LeapYearGUI {
+public class  LeapYearGUI extends JFrame{
     private JPanel panel1;
     private JTextField tfYear;
     private JButton btnCheckYear;
+
+    public static void main(String[] args) {
+        LeapYearGUI gwapo = new LeapYearGUI();
+        gwapo.setSize(600,500);
+        gwapo.setTitle("Leap Year Checker");
+        gwapo.setContentPane(gwapo.panel1);
+        gwapo.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        gwapo.setVisible(true);
+
+    }
+
+    }
 }
